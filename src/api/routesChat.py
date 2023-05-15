4f5843440aabd5cd01d83ea7ca0df0a3f1852fdf
josewdr <50268140+josewdr@users.noreply.github.com>
2023-05-14 22:55:02 -0600

    se agrega la capacidad al chatbot de responder con una imagen y una receta, además de guardar la imagen en cloudinary

--- src/api/routesChat.py
+++ src/api/routesChat.py
@@ -1,7 +1,8 @@
 import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, jsonify
+import requests
 
 # app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -26,8 +27,18 @@ from datetime import date, time, datetime, timezone, timedelta #timedelta, es pa
 from flask_jwt_extended import get_jwt
 from flask_jwt_extended import JWTManager
 
+import cloudinary
+import cloudinary.uploader
 
 chat = Blueprint('chat', __name__)
+
+# Configurar cloudinary
+cloudinary.config(
+  cloud_name = "doqx408xv",
+  api_key = "381731366278899",
+  api_secret = "88rsMojoVVbEC0IDA9HXx2rNBVU",
+  secure = True
+)
 
 
 # Handle/serialize errors like a JSON object
@@ -114,3 +125,146 @@ def open_ai():
 
 # MPT-7b : 64k tokens, ggml, q4_0, 128bits 4Q 
 # Oobaboonga, Koboldcpp
+
+@chat.route('/imageRecipe', methods=['POST'])
+def image_recipe():
+    data = request.get_json()
+    prompt = data.get('prompt', 'a white siamese cat')
+
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+
+    # Descarga la imagen
+    img_data = requests.get(image_url).content
+
+    # Consigue un timestamp y formatea como string
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Guarda la imagen en tu servidor (actualiza 'path/to/save/image' al directorio donde quieres guardar las imágenes)
+    image_path = os.path.join('src/front/img', f'{prompt.replace(" ", "_")}_{timestamp}.jpg')
+    with open(image_path, 'wb') as handler:
+        handler.write(img_data)
+
+    # Crear una nueva entrada en la base de datos
+    new_recipe_chat = RecipeChat(
+        name="nombre de la receta",  # actualiza esto
+        description="descripción de la receta",  # actualiza esto
+        user_id=1,  # actualiza esto
+        user_query=prompt,
+        image_of_recipe=image_path  # ahora esto es la ruta local de la imagen en tu servidor
+    )
+
+    # Añadir y hacer commit a la nueva entrada
+    db.session.add(new_recipe_chat)
+    db.session.commit()
+
+    # Retornar la ruta de la imagen y el ID de la receta en la respuesta
+    return jsonify({"image_path": image_path, "recipe_id": new_recipe_chat.id})
+
+@chat.route('/recipe', methods=['POST'])
+def generate_recipe():
+    data = request.get_json()
+    prompt = "Eres una pagina web de recetas que responde con descripcion de la receta, una lista de ingredientes y un paso a paso para preparar la receta solicitada por el usuario: "+ data['prompt']
+
+    # Genera la receta
+    completion = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        n=1,
+        max_tokens=1024
+    )
+    recipe_text = completion.choices[0].text
+
+    # Genera la imagen
+    response = openai.Image.create(
+        prompt=data['prompt'],
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+
+    # Descarga la imagen
+    img_data = requests.get(image_url).content
+
+    # Consigue un timestamp y formatea como string
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # Guarda la imagen en Cloudinary
+    upload_result = cloudinary.uploader.upload(
+        img_data,
+        public_id = f'{data["prompt"].replace(" ", "_")}_{timestamp}', 
+        resource_type = "auto" 
+    )
+    image_cloudinary_url = upload_result['url']
+
+    # Crear una nueva entrada en la base de datos
+    new_recipe_chat = RecipeChat(
+        name="nombre de la receta",  # actualiza esto
+        description=recipe_text,
+        user_id=1,  # actualiza esto
+        user_query=data['prompt'],
+        image_of_recipe=image_cloudinary_url  # ahora esto es la URL de la imagen en Cloudinary
+    )
+
+    # Añadir y hacer commit a la nueva entrada
+    db.session.add(new_recipe_chat)
+    db.session.commit()
+
+    # Retornar la receta, la URL de la imagen y el ID de la receta en la respuesta
+    return jsonify({"recipe": recipe_text, "image_url": image_cloudinary_url, "recipe_id": new_recipe_chat.id})
+
+
+
+# @chat.route('/recipe', methods=['POST'])
+# def generate_recipe():
+#     data = request.get_json()
+#     prompt = "Eres una pagina web de recetas que responde con descripcion de la receta, una lista de ingredientes y un paso a paso para preparar la receta solicitada por el usuario: "+ data['prompt']
+
+#     # Genera la receta
+#     completion = openai.Completion.create(
+#         engine="text-davinci-003",
+#         prompt=prompt,
+#         n=1,
+#         max_tokens=1024
+#     )
+#     recipe_text = completion.choices[0].text
+
+#     # Genera la imagen
+#     response = openai.Image.create(
+#         prompt=data['prompt'],
+#         n=1,
+#         size="1024x1024"
+#     )
+#     image_url = response['data'][0]['url']
+
+#     # Descarga la imagen
+#     img_data = requests.get(image_url).content
+
+#     # Consigue un timestamp y formatea como string
+#     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+#     # Guarda la imagen en tu servidor (actualiza 'path/to/save/image' al directorio donde quieres guardar las imágenes)
+#     image_path = os.path.join('public/img-recipe', f'{data["prompt"].replace(" ", "_")}_{timestamp}.jpg')
+#     with open(image_path, 'wb') as handler:
+#         handler.write(img_data)
+
+#     # Crear una nueva entrada en la base de datos
+#     new_recipe_chat = RecipeChat(
+#         name="nombre de la receta",  # actualiza esto
+#         description=recipe_text,
+#         user_id=1,  # actualiza esto
+#         user_query=data['prompt'],
+#         image_of_recipe=image_path  # ahora esto es la ruta local de la imagen en tu servidor
+#     )
+
+#     # Añadir y hacer commit a la nueva entrada
+#     db.session.add(new_recipe_chat)
+#     db.session.commit()
+
+#     # Retornar la receta, la ruta de la imagen y el ID de la receta en la respuesta
+#     return jsonify({"recipe": recipe_text, "image_path": image_path, "recipe_id": new_recipe_chat.id})
+
