4cb13394f682b73b1494bca874097f920c338ce7
mateo <materonlove@gmail.com>
2023-05-18 23:46:50 -0500

    Recetas arreglado

--- src/front/js/component/singleCardRecetas.jsx
+++ src/front/js/component/singleCardRecetas.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Footer } from "../component/footerSinglecardrecetas.jsx";
+
+
 
 
 
@@ -19,43 +20,48 @@ const SingleCardRecetas = ({ image, title, description, ingredients, instruction
 
 
     return (
+        <>
 
-        <div className="h-100 ">
-            <img className="rounded-4 img-fluid d-xs-none d-sm-none d-md-none " src={image} alt={title} style={{ borderRadius: "5%" }} />
+            <div className="h-100 ">
+                <img className="rounded-4 img-fluid d-xs-none d-sm-none d-md-none " src={image} alt={title} style={{ borderRadius: "5%" }} />
 
-            <div className="contenido" style={{ display: 'flex', color: 'white', paddingTop: '60px', width: '100%', border: '1px solid-succes' }}>
-                <div className="row align-items-start">
-                    <div className="col">
-                        <div style={{ flex: 1 }}>
-                            <h2 style={{ color: "white" }}>{title}</h2>
-                            <p style={{ color: "white" }}>{description}</p>
-                            <h3 style={{ color: "white" }}>Ingredientes:</h3>
-                            <ul>
-                                {ingredients.map((ingredient, index) => (
-                                    <li key={index}>{ingredient}</li>
-                                ))}
-                            </ul>
-                            <h3 style={{ color: "white" }}>Instrucciones:</h3>
-                            <ol>
-                                {instructions.map((instruction, index) => (
-                                    <li key={index}>{instruction}</li>
-                                ))}
-                            </ol>
+                <div className="contenido" style={{ display: 'flex', color: 'white', paddingTop: '40px', width: '100%', border: '1px solid-succes', paddingBottom: '15%' }}>
+                    <div className="row align-items-start">
+                        <div className="col">
+                            <div style={{ flex: 1 }}>
+                                <h2 style={{ color: "white" }}>{title}</h2>
+                                <p style={{ color: "white" }}>{description}</p>
+                                <h3 style={{ color: "white" }}>Ingredientes:</h3>
+                                <ul>
+                                    {ingredients.map((ingredient, index) => (
+                                        <li key={index}>{ingredient}</li>
+                                    ))}
+                                </ul>
+                                <h3 style={{ color: "white" }}>Instrucciones:</h3>
+                                <ol>
+                                    {instructions.map((instruction, index) => (
+                                        <li key={index}>{instruction}</li>
+                                    ))}
+                                </ol>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col align-items-end  d-none d-xs-block d-sm-block d-md-block">
-                        <img className="rounded-4 img-fluid" src={image} alt={title} style={{ borderRadius: "5%" }} />
+                        <div className="col align-items-end  d-none d-xs-block d-sm-block d-md-block ">
+                            <img className="rounded-4 img-fluid" src={image} alt={title} style={{ borderRadius: "5%" }} />
 
 
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+        </>
     );
 };
 
 const Receta = () => {
+
+
 
     const recipe = {
         image: 'https://media.istockphoto.com/id/931643150/vector/picture-icon.jpg?s=612x612&w=0&k=20&c=St-gpRn58eIa8EDAHpn_yO4CZZAnGD6wKpln9l3Z3Ok=',
@@ -84,7 +90,7 @@ const Receta = () => {
 
 
         <div style={{
-            height: "100%",
+
 
 
             justifyContent: "center",
@@ -101,6 +107,12 @@ const Receta = () => {
             />
 
         </div>
+
+
+
+
+
+
 
     );
 };
