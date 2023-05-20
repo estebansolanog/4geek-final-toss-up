f6a7f26a1ebc4f5866318792fada0270798cebd3
josewdr <50268140+josewdr@users.noreply.github.com>
2023-05-20 07:49:27 -0600

    puesta en servicio de base de datos

--- src/api/likes.py
+++ src/api/likes.py
@@ -8,16 +8,43 @@ class Like(db.Model):
     __tablename__="likes"
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer, nullable=False)
-    id_recipe = db.relationship('Recipe', backref='likes', lazy=True)
 
-    # id_user = db.Column(db.Integer, db.ForeignKey('user.id_user'), nullable=False)
-
-    # id_recipe = db.Column(db.Integer, db.ForeignKey('recipe.id_recipe'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"<Like {self.id}>"
+    
     def serialize(self):
         return {
             "id": self.id,
-            "id_recipe": self.id_recipe,
-            "number": self.number
+            "number": self.number,
+            "recipe_id": self.recipe_id,
+            "user_id": self.user_id
         }
+
+
+
+# from flask_sqlalchemy import SQLAlchemy
+# from .db import db
+# from .recipe import Recipe
+# from .user import User
+
+
+# class Like(db.Model):
+#     __tablename__="likes"
+#     id = db.Column(db.Integer, primary_key=True)
+#     number = db.Column(db.Integer, nullable=False)
+#     id_recipe = db.relationship('Recipe', backref='likes', lazy=True)
+
+#     # id_user = db.Column(db.Integer, db.ForeignKey('user.id_user'), nullable=False)
+
+#     # id_recipe = db.Column(db.Integer, db.ForeignKey('recipe.id_recipe'), nullable=False)
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "id_recipe": self.id_recipe,
+#             "number": self.number
+#         }
 
     
--- src/api/user.py
+++ src/api/user.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from .db import db
+
 from .countries import Country
+
 class User(db.Model):
     __tablename__="users"
     id = db.Column(db.Integer, primary_key=True)
@@ -8,19 +10,24 @@ class User(db.Model):
     last_name = db.Column(db.String(120), unique=False, nullable=False)
     password = db.Column(db.String(180), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
     birth_date = db.Column(db.Date, nullable=True, default=None)
     is_active = db.Column(db.Boolean(), unique=False, nullable=True)
     gender = db.Column(db.String(20), nullable=True)
     otp = db.Column(db.Integer, nullable=True)
     otp_active = db.Column(db.Boolean(), unique=False, nullable=True, default=False)
-    recipe_chat = db.relationship("RecipeChat", backref="users", lazy=True)
 
+    country_id=db.Column(db.Integer, db.ForeignKey('countries.id'))
+    
+    recipes = db.relationship("Recipe", backref="users", lazy=True)
+    likes = db.relationship("Like", backref="users", lazy=True)
+    favoritos = db.relationship("Favorito", backref="users", lazy=True)
+    coments = db.relationship("Coment", backref="users", lazy=True)
+    recipes_chat = db.relationship("RecipeChat", backref="users", lazy=True)
 
-
-    # Relación uno-a-muchos con Likes
-    # id_likes = db.relationship('Likes', backref='user', lazy=True)
-    # id_favorito = db.relationship('Favorito', backref='user', lazy=True)
-
+    def __repr__(self):
+        return f"<User {self.id}>"
+    
     def serialize(self):
         return {
             "id": self.id,
@@ -33,6 +40,50 @@ class User(db.Model):
             "gender":self.gender,
             "otp": self.otp,
             "otp_active": self.otp_active,
-            # "favoritos": self.favoritos
-            # do not serialize the password, its a security breach
+            "country_id": self.country_id,
+            "recipes": [recipe.id for recipe in self.recipes],
+            "likes": [like.id for like in self.likes],
+            "favoritos": [favorito.id for favorito in self.favoritos],
+            "coments": [coment.id for coment in self.coments],
+            "recipes_chat": [recipe_chat.id for recipe_chat in self.recipes_chat]
         }
+    
+
+# from flask_sqlalchemy import SQLAlchemy
+# from .db import db
+# from .countries import Country
+# class User(db.Model):
+#     __tablename__="users"
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(120), unique=False, nullable=False)
+#     last_name = db.Column(db.String(120), unique=False, nullable=False)
+#     password = db.Column(db.String(180), unique=False, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     birth_date = db.Column(db.Date, nullable=True, default=None)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=True)
+#     gender = db.Column(db.String(20), nullable=True)
+#     otp = db.Column(db.Integer, nullable=True)
+#     otp_active = db.Column(db.Boolean(), unique=False, nullable=True, default=False)
+#     recipe_chat = db.relationship("RecipeChat", backref="users", lazy=True)
+
+
+
+#     # Relación uno-a-muchos con Likes
+#     # id_likes = db.relationship('Likes', backref='user', lazy=True)
+#     # id_favorito = db.relationship('Favorito', backref='user', lazy=True)
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#             "last_name": self.last_name,
+#             "password": self.password,
+#             "email": self.email,
+#             "birth_date": self.birth_date,
+#             "is_active": self.is_active,
+#             "gender":self.gender,
+#             "otp": self.otp,
+#             "otp_active": self.otp_active,
+#             # "favoritos": self.favoritos
+#             # do not serialize the password, its a security breach
+#         }
